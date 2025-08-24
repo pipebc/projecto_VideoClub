37d58046a5c0feaedea145ce9bec672cf6dfa2b3
marraquetaa <167349778+marraquetaa@users.noreply.github.com>
2025-08-24 17:33:46 -0400

    Class Clientes Corregida

--- src/main/java/Clientes.java
+++ src/main/java/Clientes.java
@@ -1,59 +1,115 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Clientes{
-    private int idCliente;
-    private String Nombre;
-    private String Direccion;
-    private int Numero;
-    private List<Arriendos> arriendos;
+public class Cliente {
+    // Atributos privados (SIA1.3)
+    private String idCliente;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private boolean activo;
+    private Arriendo[] arriendos; 
+    private int numArriendos;
     
-    public Clientes (int idCliente, String Nombre, String Direccion, int Numero){
+    public Cliente(String idCliente, String nombre, String apellido, String telefono, 
+                  String email, String tipoMembresia) {
         this.idCliente = idCliente;
-        this.Nombre = Nombre;
-        this.Direccion = Direccion;
-        this.Numero = Numero;
-        this.arriendos = new ArrayList<>();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.activo = true;
+        this.arriendos = new Arriendo[100]; 
+        this.numArriendos = 0;
     }
-    public int getidCliente(){
+    
+
+    public Arriendo[] getArriendosActivos() {
+        Arriendo[] activos = new Arriendo[numArriendos];
+        for (int i = 0; i < numArriendos; i++) {
+            activos[i] = arriendos[i];
+        }
+        return activos;
+    }
+    
+    public String getIdCliente() {
         return idCliente;
     }
-    public String getNombre(){
-        return Nombre;
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
     }
-    public String getDireccion(){
-        return Direccion;
+
+    public String getNombre() {
+        return nombre;
     }
-    public int getNumero(){
-        return Numero;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public List<Arriendos> getArriendos(){
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Arriendo[] getArriendos() {
         return arriendos;
-    }   
-    public void setNombre(String nombre){
-        this.Nombre = Nombre;
     }
-    public void setDireccion(String Direccion){
-        this.Direccion  = Direccion;
-    }
-    public void setNumero(int Numero){
-        this.Numero = Numero;
+
+    public void setArriendos(Arriendo[] arriendos) {
+        this.arriendos = arriendos;
     }
     
-     public void agregarArriendo(Arriendos arriendo) {
-        this.arriendos.add(arriendo);
+    public int getNumArriendos() {
+        return numArriendos;
     }
-     
-    // Sobrecarga 1: Agregar arriendo con datos básicos
-    public void agregarArriendo(String pelicula, String fechaArriendo) {
-        Arriendos nuevoArriendo = new Arriendos("", pelicula, fechaArriendo, "");
-        this.arriendos.add(nuevoArriendo);
+
+    public boolean agregarArriendo(Arriendo arriendo) {
+        if (numArriendos < arriendos.length) {
+            arriendos[numArriendos] = arriendo;
+            numArriendos++;
+            return true;
+        }
+        return false;
     }
     
-    // Sobrecarga 2: Agregar arriendo completo
-    public void agregarArriendo(String sugerencias, String pelicula, String fechaArriendo, String fechaDevolucion) {
-        Arriendos nuevoArriendo = new Arriendos(sugerencias, pelicula, fechaArriendo, fechaDevolucion);
-        this.arriendos.add(nuevoArriendo);
+    public void actualizarContacto(String nuevoTelefono) {
+        this.telefono = nuevoTelefono;
+        System.out.println("Teléfono actualizado: " + nuevoTelefono);
     }
+    
+    public void actualizarContacto(String nuevoTelefono, String nuevoEmail) {
+        this.telefono = nuevoTelefono;
+        this.email = nuevoEmail;
+        System.out.println("Contactos actualizados: " + nuevoTelefono + ", " + nuevoEmail);
+    }
+    
 }
+
 
