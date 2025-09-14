fa02d69e95b18d7c74a84a3b247c764de66c1894
pipebc <felipebarrazacarvajal06@gmail.com>
2025-09-14 19:51:37 -0300

    Update Pelicula.java

--- src/main/java/Pelicula.java
+++ src/main/java/Pelicula.java
@@ -6,8 +6,7 @@ public class Pelicula {
     private int stock;
     private int vecesArrendada;
 
-    public Pelicula(String idPelicula, String titulo, String genero, String director, 
-                   int stock, String clasificacion) {
+    public Pelicula(String idPelicula, String titulo, String genero, String director, int stock) {
         this.idPelicula = idPelicula;
         this.titulo = titulo;
         this.genero = genero;
@@ -16,56 +15,7 @@ public class Pelicula {
         this.vecesArrendada = 0;
     }
 
-
-    public String getIdPelicula() {
-        return idPelicula;
-    }
-
-    public void setIdPelicula(String idPelicula) {
-        this.idPelicula = idPelicula;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public int getVecesArrendada() {
-        return vecesArrendada;
-    }
-
-    public void setVecesArrendada(int vecesArrendada) {
-        this.vecesArrendada = vecesArrendada;
-    }
-
-
+   
     public boolean arrendar() {
         if (stock > 0) {
             stock--;
@@ -77,5 +27,43 @@ public class Pelicula {
 
     public void devolver() {
         stock++;
+    }
+
+    
+    public String getIdPelicula() { 
+        return idPelicula; 
+    }
+    public void setIdPelicula(String idPelicula) { 
+        this.idPelicula = idPelicula; 
+    }
+    public String getTitulo() { 
+        return titulo; 
+    }
+    public void setTitulo(String titulo) { 
+        this.titulo = titulo; 
+    }
+    public String getGenero() { 
+        return genero; 
+    }
+    public void setGenero(String genero) { 
+        this.genero = genero; 
+    }
+    public String getDirector() { 
+        return director; 
+    }
+    public void setDirector(String director) { 
+        this.director = director; 
+    }
+    public int getStock() { 
+        return stock; 
+    }
+    public void setStock(int stock) { 
+        this.stock = stock; 
+    }
+    public int getVecesArrendada() { 
+        return vecesArrendada; 
+    }
+    public void setVecesArrendada(int vecesArrendada) { 
+        this.vecesArrendada = vecesArrendada; 
     }
 }
