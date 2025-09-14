e8e0bb769e1e4c164e0397bd3f6515033fbec40f
pipebc <felipebarrazacarvajal06@gmail.com>
2025-09-14 19:52:07 -0300

    Update Cliente.java

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -7,8 +7,8 @@ public class Cliente {
     private String telefono;
     private String email;
     private boolean activo;
-    private List<Arriendo> arriendos; 
-    
+    private List<Arriendo> arriendos;
+
     public Cliente(String idCliente, String nombre, String apellido, String telefono, String email) {
         this.idCliente = idCliente;
         this.nombre = nombre;
@@ -16,71 +16,21 @@ public class Cliente {
         this.telefono = telefono;
         this.email = email;
         this.activo = true;
-        this.arriendos = new ArrayList<>(); 
+        this.arriendos = new ArrayList<>();
     }
 
-    public List<Arriendo> getArriendosActivos() {
-        List<Arriendo> activos = new ArrayList<>();
-        for (Arriendo a : arriendos) {
-            activos.add(a);
-        }
-        return activos;
-    }
     
-    public String getIdCliente() {
-        return idCliente;
-    }
-
-    public void setIdCliente(String idCliente) {
-        this.idCliente = idCliente;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
-    public List<Arriendo> getArriendos() {
-        return arriendos;
-    }
-
     public boolean agregarArriendo(Arriendo arriendo) {
         return arriendos.add(arriendo);
+    }
+
+    public Arriendo buscarArriendoPorId(String idArriendo) {
+        for (Arriendo a : arriendos) {
+            if (a.getIdArriendo().equals(idArriendo)) {
+                return a;
+            }
+        }
+        return null;
     }
 
     public boolean eliminarArriendoPorId(String idArriendo) {
@@ -95,18 +45,65 @@ public class Cliente {
         return false;
     }
 
+    public List<Arriendo> getArriendosActivos() {
+        List<Arriendo> activos = new ArrayList<>();
+        for (Arriendo a : arriendos) {
+            if (!a.isDevuelto()) {
+                activos.add(a);
+            }
+        }
+        return activos;
+    }
+
+    
+    public String getIdCliente() { 
+        return idCliente; 
+    }
+    public void setIdCliente(String idCliente) { 
+        this.idCliente = idCliente; 
+    }
+    public String getNombre() { 
+        return nombre; 
+    }
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+    public String getApellido() { 
+        return apellido; 
+    }
+    public void setApellido(String apellido) { 
+        this.apellido = apellido; 
+    }
+    public String getTelefono() { 
+        return telefono; 
+    }
+    public void setTelefono(String telefono) { 
+        this.telefono = telefono; 
+    }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    public boolean isActivo() { 
+        return activo; 
+    }
+    public void setActivo(boolean activo) { 
+        this.activo = activo; 
+    }
+    public List<Arriendo> getArriendos() { 
+        return arriendos; 
+    }
+
     public void actualizarContacto(String nuevoTelefono) {
         this.telefono = nuevoTelefono;
         System.out.println("Teléfono actualizado: " + nuevoTelefono);
     }
-    
+
     public void actualizarContacto(String nuevoTelefono, String nuevoEmail) {
         this.telefono = nuevoTelefono;
         this.email = nuevoEmail;
         System.out.println("Contactos actualizados: " + nuevoTelefono + ", " + nuevoEmail);
     }
-    
 }
-
-
-
