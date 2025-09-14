5ec609aa84c94cfa8a570a4878d33cb7773d72de
pipebc <felipebarrazacarvajal06@gmail.com>
2025-09-14 19:52:35 -0300

    Update Arriendo.java

--- src/main/java/Arriendo.java
+++ src/main/java/Arriendo.java
@@ -5,7 +5,7 @@ public class Arriendo {
     private String fechaArriendo;
     private String fechaDevolucion;
     private boolean devuelto;
-    private int calificacion; 
+    private int calificacion;
 
     public Arriendo(String idArriendo, Cliente cliente, Pelicula pelicula, String fechaArriendo) {
         this.idArriendo = idArriendo;
@@ -16,59 +16,56 @@ public class Arriendo {
         this.calificacion = 0;
     }
 
-    public String getIdArriendo() {
-        return idArriendo;
-    }
-
-    public void setIdArriendo(String idArriendo) {
-        this.idArriendo = idArriendo;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Pelicula getPelicula() {
-        return pelicula;
-    }
-
-    public void setPelicula(Pelicula pelicula) {
-        this.pelicula = pelicula;
-    }
-
-    public String getFechaArriendo() {
-        return fechaArriendo;
-    }
-
-    public void setFechaArriendo(String fechaArriendo) {
-        this.fechaArriendo = fechaArriendo;
-    }
-
-    public String getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(String fechaDevolucion) {
+    
+    public void marcarDevuelto(String fechaDevolucion) {
+        this.devuelto = true;
         this.fechaDevolucion = fechaDevolucion;
+        if (pelicula != null) {
+            pelicula.devolver();
+        }
     }
 
-    public boolean isDevuelto() {
-        return devuelto;
+    
+    public String getIdArriendo() { 
+        return idArriendo; 
     }
-
-    public void setDevuelto(boolean devuelto) {
-        this.devuelto = devuelto;
+    public void setIdArriendo(String idArriendo) { 
+        this.idArriendo = idArriendo; 
     }
-
-    public int getCalificacion() {
-        return calificacion;
+    public Cliente getCliente() { 
+        return cliente; 
     }
-
-    public void setCalificacion(int calificacion) {
-        this.calificacion = calificacion;
+    public void setCliente(Cliente cliente) { 
+        this.cliente = cliente; 
+    }
+    public Pelicula getPelicula() { 
+        return pelicula; 
+    }
+    public void setPelicula(Pelicula pelicula) { 
+        this.pelicula = pelicula; 
+    }
+    public String getFechaArriendo() { 
+        return fechaArriendo; 
+    }
+    public void setFechaArriendo(String fechaArriendo) { 
+        this.fechaArriendo = fechaArriendo; 
+    }
+    public String getFechaDevolucion() { 
+        return fechaDevolucion; 
+    }
+    public void setFechaDevolucion(String fechaDevolucion) { 
+        this.fechaDevolucion = fechaDevolucion; 
+    }
+    public boolean isDevuelto() { 
+        return devuelto; 
+    }
+    public void setDevuelto(boolean devuelto) { 
+        this.devuelto = devuelto; 
+    }
+    public int getCalificacion() { 
+        return calificacion; 
+    }
+    public void setCalificacion(int calificacion) { 
+        this.calificacion = calificacion; 
     }
 }
